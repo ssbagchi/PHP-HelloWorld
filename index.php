b701536169f9983db18babeab18dc8e61e188920
ssbagchi <39180998+ssbagchi@users.noreply.github.com>
2019-01-22 16:59:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World, this is web hooked CICD</p>'; ?> 
+ <?php echo '<p>Hello World, this is web hooked CICD  2nd build</p>'; ?> 
  </body>
 </html>
